6da145bc6636857e308660525c32eeca6d991dee
Ezi99 <ismaeljubranja@mta.ac.il>
2024-10-29 16:31:25 +0200

    refactor + new classes
    enhanced: App class will now handle map loading and user interaction.
    
    added: Workout, Running, Cycling classes.

--- script.js
+++ script.js
@@ -10,63 +10,125 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
-const geoNav = navigator.geolocation;
 
-let map;
-let mapEvent;
+class App {
+  #map;
+  #mapEvent;
+  #workouts;
 
-if (geoNav) {
-  geoNav.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      const coords = [latitude, longitude];
+  constructor() {
+    this._getPosition();
+    form.addEventListener("submit", this._newWorkout.bind(this));
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
 
-      map = L.map("map").setView(coords, 13);
-
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      map.on("click", function (event) {
-        mapEvent = event;
-        form.classList.remove("hidden");
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert("couldn't get you position");
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("couldn't get you position");
+        }
+      );
     }
-  );
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    this.#map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(event) {
+    this.#mapEvent = event;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  _newWorkout(event) {
+    event.preventDefault();
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        " ";
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("workout")
+      .openPopup();
+  }
 }
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const { lat, lng } = mapEvent.latlng;
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+  distance;
+  duration;
+  coords;
 
-  inputCadence.value =
-    inputDistance.value =
-    inputDuration.value =
-    inputElevation.value =
-      " ";
+  constructor(distance, duration, coords) {
+    this.distance = distance;
+    this.duration = duration;
+    this.coords = coords;
+  }
+}
 
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    .setPopupContent("workout")
-    .openPopup();
-});
+class Running extends Workout {
+  #cadence;
+  #pace;
 
-inputType.addEventListener("change", function () {
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+  constructor(distance, duration, coords, cadence) {
+    super(distance, duration, coords);
+    this.#cadence = cadence;
+    this._calcPace();
+  }
+
+  _calcPace() {
+    this.#pace = this.duration / this.distance;
+    return this.#pace;
+  }
+}
+
+class Cycling extends Workout {
+  #elevationGain;
+  #speed;
+
+  constructor(distance, duration, coords, elevationGain) {
+    super(distance, duration, coords);
+    this.#elevationGain = elevationGain;
+    this._calcSpeed();
+  }
+
+  _calcSpeed() {
+    this.#speed = this.distance / (this.duration / 60);
+    return this.#speed;
+  }
+}
+
+const app = new App();
